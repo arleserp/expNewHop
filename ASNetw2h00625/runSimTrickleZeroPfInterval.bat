d027c28eced6361b69993274f8d45746061c317d
arleserp <arles.rodriguez@gmail.com>
2021-10-13 12:11:03 -0500

    Experiments with 2h

---
+++ ASNetw2h00625/runSimTrickleZeroPfInterval.bat
@@ -0,0 +1,18 @@
+for /L %%x in (1, 1, 30) do (
+
+Java -d64 -Xms3G -Xmx3G  -XX:+UseG1GC  -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 -XX:InitiatingHeapOccupancyPercent=70  -classpath dist/NetworkRecoverySim.jar networkrecoverysim.DataReplicationNodeFailingMain load as19981231.graph 0 0 carriersrep as19981231.loc 80 replalgon NODELAY  0.5 4 trickle 4 3 0 zeropf-30-55
+
+REM pause
+REM load as19981231.graph 0 0 carriersrep as19981231.graph 1500 replalgon NODELAY  0.5 4 allinfo 4 5 0 backtolowpf1000
+
+REM Java -d64 -Xms3G -Xmx3G  -XX:+UseG1GC  -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 -XX:InitiatingHeapOccupancyPercent=70  -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.DataReplicationNodeFailingMain load scalefree+sn+4+eta+1+numSt+97.graph 0 0 random scalefree+sn+4+eta+1+numSt+97.loc 6000 replalgon NODELAY  0.25 8 nhopsinfo 8 5 0 backtolowpf5000
+
+	
+REM Java -d64 -Xms3G -Xmx3G  -XX:+UseG1GC  -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 -XX:InitiatingHeapOccupancyPercent=70  -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.DataReplicationNodeFailingMain load circlelonghubandspoke+v+100+l+2.graph 0 0 random circlelonghubandspoke+v+100+l+2.loc 10000 replalgon NODELAY  0.25 4 nhopsinfo 4 5 0 backtolowpf5000
+
+
+REM Java -d64 -Xms3G -Xmx3G  -XX:+UseG1GC  -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 -XX:InitiatingHeapOccupancyPercent=70  -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.DataReplicationNodeFailingMain load longhubandspoke+v+100+l+2.graph 0 0 random longhubandspoke+v+100+l+2.loc 10000 replalgon NODELAY  0.25 4 nhopsinfo 4 5 0 backtolowpf5000
+
+REM Java -d64 -Xms3G -Xmx3G  -XX:+UseG1GC  -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 -XX:InitiatingHeapOccupancyPercent=70  -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.DataReplicationNodeFailingMain load longhubandspoke+v+100+l+3.graph 0 0 random longhubandspoke+v+100+l+3.loc 10000 replalgon NODELAY  0.25 4 nhopsinfo 4 5 0 backtolowpf5000
+
+)
