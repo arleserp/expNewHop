80b9051b23cbaa6dbd568f9ac3b4b8e3e6469234
arleserp <arles.rodriguez@gmail.com>
2021-10-14 16:55:24 -0500

    Adicion de red scalefree

---
+++ Scalefreehops3/FailSim.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+i=1
+
+while [ $i -le 71 ]; do
+    echo $i
+    i=$(($i+1))
+	Java -Xms3G -Xmx3G  -XX:+UseG1GC  -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 -XX:InitiatingHeapOccupancyPercent=70  -classpath dist/NetworkRecoverySim.jar networkrecoverysim.DataReplicationNodeFailingMain load scalefree+sn+4+eta+1+numSt+97.graph 100 0 FirstNeighbor scalefree+sn+4+eta+1+numSt+97.loc 50 replalgon NODELAY  0.125 4 nhopsinfo 3 3 0 FailIntv-0-25
+done
---
+++ Scalefreehops3/generateLocations.bat
@@ -0,0 +1,2 @@
+java   -classpath dist/NetworkRecoverySim.jar mobileagents.LocationService . 512
+pause
