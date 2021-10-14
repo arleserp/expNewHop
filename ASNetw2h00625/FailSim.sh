daecc6847c7ef48e51b745ab0d95919545674764
arleserp <arles.rodriguez@gmail.com>
2021-10-13 22:08:52 -0500

    first experiment with huge network

--- ASNetw2h00625/FailSim.sh
+++ ASNetw2h00625/FailSim.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 i=1
-while [ $i -le 30 ]; do
+
+while [ $i -le 71 ]; do
     echo $i
     i=$(($i+1))
-	java -d64 -Xms3G -Xmx6G  -XX:+UseG1GC  -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 -XX:InitiatingHeapOccupancyPercent=70  -classpath dist/NetworkRecoverySim.jar networkrecoverysim.DataReplicationNodeFailingMain load as19981231.graph 0 0 FirstNeighbor as19981231.loc 50 replalgon NODELAY 0.0625 4 nhopsinfo 2 3 0 FailIntv-0-25
+	java  -Xms3G -Xmx6G  -XX:+UseG1GC  -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 -XX:InitiatingHeapOccupancyPercent=70  -classpath dist/NetworkRecoverySim.jar networkrecoverysim.DataReplicationNodeFailingMain load as19981231.graph 0 0 FirstNeighbor as19981231.loc 50 replalgon NODELAY 0.0625 4 nhopsinfo 2 3 0 FailIntv-0-25
 done
